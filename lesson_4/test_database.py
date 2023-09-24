624941138bae4038799a0a500e573199e1587919
Anastasia Ageeva <anastasia.smirnova@gmail.com>
2023-09-24 18:08:03 +0300

    added task_5, task_6 and task_7 to lesson_4

---
+++ lesson_4/test_database.py
@@ -0,0 +1,13 @@
+import unittest
+from unittest.mock import Mock
+
+from task_6_database import Database, DataProcessor
+
+
+class TestDataProcessor(unittest.TestCase):
+    def setUp(self) -> None:
+        self.data_processor = DataProcessor(Mock())
+
+    def test_data_processor(self):
+        result = self.data_processor.db_query('select * from abstract_table')
+        self.data_processor.database.query.assert_called_once()
---
+++ lesson_4/test_http.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest.mock import Mock
+
+from task_7_http import HttpClient, WebService
+
+
+class TestWebService(unittest.TestCase):
+    def setUp(self) -> None:
+        self.webservice = WebService(Mock())
+
+    def test_make_request(self):
+        response = self.webservice.make_request('https://google.com', 'weather')
+        self.webservice.http_client.get.assert_called()
+
+    def test_no_make_put_request(self):
+        response = self.webservice.make_request('https://google.com', 'weather')
+        self.webservice.http_client.put.assert_not_called()
